970cc780462245553baf43cc48b933f3d74eb814
Venkata Rama Sai Badam <vbadam@atrihub.io>
2025-04-07 17:22:10 -0700

    feat (Chapter 4) : Tests and Refactoring [ICTRAINING-1954]

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,7 @@
 from django.test import TestCase
-from django.http import HttpRequest
-from lists.views import home_page
+
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get("/")   
-        html = response.content.decode("utf8")  
-        self.assertIn("<title>To-Do lists</title>", html)  
-        self.assertTrue(html.startswith("<html>"))  
-        self.assertTrue(html.endswith("</html>")) 
-
-
+    def test_uses_home_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
